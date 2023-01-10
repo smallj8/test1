dd1e0b313923b1d922bfacd9d2b9b14c583babc7
smallj8 <guiling6668@gmail.com>
2023-01-10 10:12:15 +0800

    init project

---
+++ pages/home/home.vue
@@ -0,0 +1,25 @@
+<template>
+	<view>
+		home
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+	.funky {
+	  font: {
+		family: fantasy;
+		size: 30em;
+		weight: bold;
+	  }
+	}
+</style>
